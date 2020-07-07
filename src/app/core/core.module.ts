a3522f4cf70b9cbc77b948e68a6eaaf6a4e0e933
christophe gueroult <christophegueroult@gmail.com>
2020-07-07 11:32:57 +0100

    ui and transclusions

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,11 +4,14 @@ import { LoginModule } from '../login/login.module';
 import { TextModule } from '../text/text.module';
 import { UiModule } from '../ui/ui.module';
 import { IconsModule } from '../icons/icons.module';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, NavComponent, FooterComponent],
   imports: [
     CommonModule,
     LoginModule,
@@ -20,7 +23,10 @@ import { IconsModule } from '../icons/icons.module';
     LoginModule,
     TextModule,
     UiModule,
-    IconsModule
+    IconsModule,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent
   ]
 })
 export class CoreModule { }
