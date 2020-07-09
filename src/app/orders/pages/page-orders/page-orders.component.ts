8b46f0ab002890aa91c8f01ec4c4b768110e034e
christophe gueroult <christophegueroult@gmail.com>
2020-07-09 11:31:20 +0100

    pipe async

--- src/app/orders/pages/page-orders/page-orders.component.ts
+++ src/app/orders/pages/page-orders/page-orders.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { OrdersService } from '../../services/orders.service';
 import { Order } from 'src/app/shared/models/order';
+import { Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-page-orders',
   templateUrl: './page-orders.component.html',
   styleUrls: ['./page-orders.component.scss']
 })
-export class PageOrdersComponent implements OnInit {
-  public collection: Order[];
+export class PageOrdersComponent implements OnInit, OnDestroy {
+  // public collection: Order[];
+  public collection$: Observable<Order[]>;
   public title: string;
   public headers: string[];
   color = 'white';
+  // private sub: Subscription;
   constructor(private os: OrdersService) { }
 
   ngOnInit(): void {
@@ -25,9 +28,14 @@ export class PageOrdersComponent implements OnInit {
       'State',
     ];
     this.title = 'Orders List';
-    this.os.collection.subscribe((datas) => {
-      this.collection = datas;
-    });
+    this.collection$ = this.os.collection;
+    // this.sub = this.os.collection.subscribe((datas) => {
+    //   this.collection = datas;
+    // });
+  }
+
+  ngOnDestroy() {
+    // this.sub.unsubscribe();
   }
 
 }
