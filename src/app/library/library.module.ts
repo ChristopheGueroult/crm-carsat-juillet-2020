04ed5ef99e130899e54a142272c987cf47f14845
christophe gueroult <christophegueroult@gmail.com>
2020-07-10 08:42:29 +0100

    table dark

--- src/app/library/library.module.ts
+++ src/app/library/library.module.ts
@@ -3,15 +3,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { BtnComponent } from './components/btn/btn.component';
 import { TableLightComponent } from './components/table-light/table-light.component';
+import { TableDarkComponent } from './components/table-dark/table-dark.component';
 
 
 
 @NgModule({
-  declarations: [TableLightComponent, BtnComponent],
+  declarations: [TableLightComponent, BtnComponent, TableDarkComponent],
   imports: [
     CommonModule,
     RouterModule
   ],
-  exports: [TableLightComponent, BtnComponent]
+  exports: [TableLightComponent, BtnComponent, TableDarkComponent]
 })
 export class LibraryModule { }
